f5dbc4a62be04a5f5eff43f264fb44510eb69063
Mihajlo Madic <mihajlo.madic@elfak.rs>
2023-12-21 01:09:49 +0100

    Project initialization

---
+++ e-shop-api/src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      ignoreEnvFile: false,
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host:process.env.POSTGRES_HOST,
+      port:parseInt(process.env.POSTGRES_PORT),
+      username:process.env.POSTGRES_USER,
+      password:process.env.POSTGRES_PASSWORD,
+      database:process.env.POSTGRESS_DATABASE,
+      autoLoadEntities:true,
+      synchronize:true,
+    }),
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor() {
+    console.log(process.env.POSTGRES_HOST)
+  }
+}
---
+++ e-shop-api/src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import Redis from 'ioredis';
+
+@Injectable()
+export class AppService {
+  async getPong(): Promise<string> {
+    const redis_client = new Redis({host: "localhost", port: 6379})
+
+    return redis_client.ping()
+  }
+}
