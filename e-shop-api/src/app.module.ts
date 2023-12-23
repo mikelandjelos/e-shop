4a67c9c8b1f5e89990154bac8763b6b694b7631a
Mihajlo Madic <mihajlo.madic@elfak.rs>
2023-12-23 21:01:52 +0100

    Added customer resource, deleted things, etc.

--- e-shop-api/src/app.module.ts
+++ e-shop-api/src/app.module.ts
@@ -3,32 +3,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.development.env',
       ignoreEnvFile: false,
-      isGlobal: true
+      isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-      type:'postgres',
-      host:process.env.POSTGRES_HOST,
-      port:parseInt(process.env.POSTGRES_PORT),
-      username:process.env.POSTGRES_USER,
-      password:process.env.POSTGRES_PASSWORD,
-      database:process.env.POSTGRESS_DATABASE,
-      autoLoadEntities:true,
-      synchronize:true,
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRESS_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
-    UserModule
+    CustomerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
   constructor() {
-    console.log(process.env.POSTGRES_HOST)
+    console.log(process.env.POSTGRES_HOST);
   }
 }
