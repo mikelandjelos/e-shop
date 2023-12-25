9e5d417e5bbf92247bddee363b0e000cd9952dcb
Mihajlo Madic <mihajlo.madic@elfak.rs>
2023-12-25 17:44:01 +0100

    Branches merge

--- e-shop-api/src/main.ts
+++ e-shop-api/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:4200', 
+    origin: 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
