5a6451fb400dd752d7a310cc306ac0423b03a19a
VVeljovic <veljkoveljovic13@gmail.com>
2024-01-06 16:25:56 +0100

    create

---
+++ e-shop-client/src/app/create-product-form/create-product-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-product-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-product-form.component.html',
+  styleUrl: './create-product-form.component.css'
+})
+export class CreateProductFormComponent {
+
+}
