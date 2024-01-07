0362b92f561eb78dad343c2734d5daf3996d60ca
VVeljovic <veljkoveljovic13@gmail.com>
2024-01-07 19:15:57 +0100

    filters

--- e-shop-client/src/app/app.routes.ts
+++ e-shop-client/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { GeoComponent } from './geo/geo.component';
 import { CreateProductFormComponent } from './create-product-form/create-product-form.component';
+import { FiltersComponent } from './filters/filters.component';
 
 export const routes: Routes = [
   { path: 'login', pathMatch: 'full', component: LoginComponent },
   { path: 'geo', pathMatch: 'full', component: GeoComponent },
   { path: 'front-page', pathMatch: 'full', component: FrontPageComponent },
   { path: '', redirectTo: '/front-page', pathMatch: 'full' },
-  { path: 'c', pathMatch: 'full', component: CreateProductFormComponent },
+  { path: 'c', pathMatch: 'full', component: FiltersComponent },
 ];
